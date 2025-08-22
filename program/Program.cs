932e27fdd568c12669a16a1faa7bacfb7c38f57b
Israel <israelvictor.contact@gmail.com>
2025-08-22 15:09:37 -0300

    Meu primeiro programa em Csharp

---
+++ program/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "";
+            Console.WriteLine("Escreva seu nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Seu nome é: ");
+            Console.WriteLine(nome);
+
+            Console.ReadLine();
+
+        }
+    }
+}
